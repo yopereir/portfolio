c7d5e16a4f915b063640690b8463b0056e8c4fe9
ypereira <ypereira@bgea.org>
2022-06-09 13:38:17 -0400

    added function to send email and redirect to thankyou page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -2,7 +2,7 @@
 import * as React from "react"
 import { Flex, jsx, Container, Heading, Themed, useColorMode } from "theme-ui"
 import { animated, useSpring, config } from "react-spring"
-import { useStaticQuery, graphql, Link } from "gatsby"
+import { useStaticQuery, graphql, Link, navigate } from "gatsby"
 import { MDXRenderer } from "gatsby-plugin-mdx"
 import { GatsbyImage, IGatsbyImageData } from "gatsby-plugin-image"
 import HeaderBackground from "../components/header-background"
@@ -10,6 +10,7 @@ import LeftArrow from "../assets/left-arrow"
 import useEmiliaConfig from "../hooks/use-emilia-config"
 import ColorModeToggle from "../components/colormode-toggle"
 import styles from "./css/contact.css"
+import fetch from 'node-fetch';
 
 type ContactPageProps = {
   title: string
@@ -24,6 +25,15 @@ type AvatarStaticQuery = {
       gatsbyImageData: IGatsbyImageData
     }
   }
+}
+
+const allowOnlyValidCharacters = (subjectString: String) => {
+  let parsedString = ""
+  for (let x = 0; x < subjectString.length; x++)
+  {
+    parsedString = parsedString.concat(("abcdefghijklmnopqrstuvwxyz.,;:! 1234567890@".includes(subjectString.charAt(x).toLowerCase()))?subjectString.charAt(x):"")
+  }
+  return parsedString
 }
 
 const ContactPage = ({ title, areas, description = ``, date }: ContactPageProps) => {
@@ -56,6 +66,19 @@ const ContactPage = ({ title, areas, description = ``, date }: ContactPageProps)
   })
   const infoProps = useSpring({ config: config.slow, delay: 500, from: { opacity: 0 }, to: { opacity: 1 } })
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch("https://script.google.com/macros/s/AKfycbyHyCc0zzdfJQ0MSlmTqIs3Pz22VWqOnx0sYgK5H1wpIssO9bxIvrlzUNq9YDCIYBwT/exec", {
+      "headers": {"content-type": "application/x-www-form-urlencoded",},
+      "body": "name="+encodeURIComponent(allowOnlyValidCharacters(e.target.name.value))
+      +"&companyname="+encodeURIComponent(allowOnlyValidCharacters(e.target.companyname.value))
+      +"&email="+encodeURIComponent(allowOnlyValidCharacters(e.target.email.value))
+      +"&phonenumber="+encodeURIComponent(allowOnlyValidCharacters(e.target.phonenumber.value))
+      +"&message="+encodeURIComponent(allowOnlyValidCharacters(e.target.message.value)),
+      "method": "POST"
+    });
+    navigate("/thankyou");
+  };
   return (
     <Flex as="header" variant="layout.header">
       <div style={{ top: "1rem", right: "1rem", position: "absolute", "textAlign": `right`}}><ColorModeToggle isDark={isDark} toggle={toggleColorMode} /></div>
@@ -102,7 +125,7 @@ const ContactPage = ({ title, areas, description = ``, date }: ContactPageProps)
           </animated.div>
           <animated.div style={infoProps}>
             <Themed.p sx={{ mb: 0, mt: 4 }}>{date}</Themed.p>
-<form class="gform" method="POST" data-email="yohann.pereira28@gmail.com" action="https://script.google.com/macros/s/AKfycbyHyCc0zzdfJQ0MSlmTqIs3Pz22VWqOnx0sYgK5H1wpIssO9bxIvrlzUNq9YDCIYBwT/exec">
+<form class="gform" method="POST" data-email="yohann.pereira28@gmail.com" onSubmit={handleSubmit}>
   <div class="container" style={{styles}}>
     <div class="row">
         <h4 style={{"text-align":"center"}}>We'd love to hear from you!</h4>
